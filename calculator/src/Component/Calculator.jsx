cbb562213a754d0bd3032302b1b0c421bb169e4f
Soundradevi-S-R <soundradevi.rajagopal@gmail.com>
2024-09-07 18:55:17 +0530

    calculator-completed

---
+++ calculator/src/Component/Calculator.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+
+import style from "./Calculator.module.css";
+
+
+function Calculator(){
+
+    const [input,setInput] = useState("");
+    const [output,setOutput] = useState("");
+
+
+const number_arr =['7','8','9','+','4','5','6','-','1','2','3','*','C','0','=','/'];
+
+
+
+const calculate = (val) =>{
+
+    if(  val === '' ){
+        if( val === '='){
+         setOutput('Error');
+        }
+    }else{
+        setInput(prevInput=> prevInput+val);  
+    }
+    if( val === '='){
+        try{
+            setInput(input);
+            setOutput(eval(input));
+        }catch(e){
+            setInput('');
+            alert("Error/Invalid expression");
+        }
+       
+    }
+    if( val === 'C'){
+        setInput('');
+        setOutput('');
+    }
+    //else{
+       // setInput(prevInput=> prevInput+val); //adding val [new value] to existing input 
+       // setOutput(eval(input));
+   // }
+
+
+}
+
+return (
+    <div className={style.wrapperDiv}>
+        <div className={style.calculator_Div} >
+
+            <h1>React Calculator</h1>
+            <input 
+                type="text" 
+                value={input} 
+                onChange={()=>setInput(input)}
+                className={style.input_style}/>
+
+            <div className={style.outputDiv}>{output}</div>
+
+            <div className={style.calc_div}>
+
+                { number_arr.map((ele) =>{
+               
+                    return (<button 
+                              key={ele}
+                              className={style.calc_button}
+                              onClick={ ()=>calculate(ele) } >{ele}
+                              </button>);
+                            })                
+                }   
+                
+            </div>
+        </div>
+    </div>);
+
+
+}
+export default Calculator;
